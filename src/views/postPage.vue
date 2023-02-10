f048a34e974bbb55b9fa53d3edce973a7e4d9e63
GabrieleCiurlionyte <ciurlionyte.gabriele@gmail.com>
2023-02-10 14:11:20 +0200

    Implemented basic Search
    #BUG - pagination not working for search query

--- src/views/postPage.vue
+++ src/views/postPage.vue
@@ -2,7 +2,11 @@
     <div id="Page">
         <search-bar id="search-bar">
           <template #input-slot>
-            <input class="input" type="search" placeholder="Search..."/>
+            <input class="input" type="search" placeholder="Search..."
+            v-model.lazy.trim="searchQuery"
+            @focusout="getSearchQuery(1)"
+            @keyup.enter="getSearchQuery(1)"
+            />
           </template>
           <template #icon-slot>
             <i class="fa-solid fa-magnifying-glass"></i>
@@ -39,9 +43,12 @@ export default {
   data () {
     return {
       posts: [],
+      POSTS_PER_PAGE : 5,
       current_page : 1,
       //TODO fix hardcoding
       last_page : 5,
+      searchMode : false,
+      searchQuery : ""
     };
   },
 
@@ -49,6 +56,7 @@ export default {
   },
 
   created () {
+    this.searchMode = false;
     this.getData(0);
   },
 
@@ -57,12 +65,13 @@ export default {
   },
   
   methods: {
-    //Method overloading
+
+    //TODO: whenever we clear the input field we go back to normal display mode or refresh page
+    
     async getData(pageNumber) {
-      const POSTS_PER_PAGE = 5;
       try {
         const response = await this.$http.get(
-          `http://localhost:3000/Articles?_page=${pageNumber}&_limit=${POSTS_PER_PAGE}`
+          `http://localhost:3000/Articles?_page=${pageNumber}&_limit=${this.POSTS_PER_PAGE}`
         );
         this.posts = response.data;
         console.log(this.posts);
@@ -71,26 +80,58 @@ export default {
       }
     },
 
+
+    async getSearchQuery(pageNumber) {
+      if(this.searchQuery === ""){
+        this.searchMode = false;
+        this.current_page = 1;
+        this.ExecuteAPICall();
+      }
+      try {
+        this.searchMode = true;
+        const response = await this.$http.get(
+          `http://localhost:3000/Articles?q=${this.searchQuery}&_page=${pageNumber}&_limit=${this.POSTS_PER_PAGE}`
+        );
+        this.posts = response.data;
+        this.current_page = 1;
+        console.log(this.posts);
+      } catch (error) {
+        this.searchMode = false;
+        console.log(error);
+      }
+    },
+
+    ExecuteAPICall() {
+      if(!(this.searchMode))
+      {
+        this.getData(this.current_page);
+      }
+      else
+      {
+        this.getSearchQuery(this.current_page, this.searchQuery);
+      }
+    },
+
     GoToNextPage(){
       this.current_page += 1;
-      this.getData(this.current_page);
+      this.ExecuteAPICall();
     },
 
     GoToPreviousPage(){
       if(this.current_page != 1){
         this.current_page -= 1;
-        this.getData(this.current_page);
+        this.ExecuteAPICall();
       }  
     },
 
     GoToFirstPage(){
         this.current_page = 1;
-        this.getData(this.current_page);
+        this.ExecuteAPICall();
     },
 
     GoToLastPage(){
         this.current_page = this.last_page;
-        this.getData(this.current_page); 
+        this.ExecuteAPICall();
     }
 
   },
